182b920fd68cc1072de840da5727751db45964f1
SagarPramod <sagarpramod01@gmail.com>
2024-11-09 12:30:52 +0530

    added terraform code in installations.sh

--- Jenkins-TF/installations.sh
+++ Jenkins-TF/installations.sh
@@ -33,3 +33,11 @@ sudo apt-get install trivy -y
  sudo apt install curl
  curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
  sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+#install terraform
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install terraform
