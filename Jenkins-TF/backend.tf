ef7b38dc04def5ad02bdbf182f05e8ce6dde499b
SagarPramod <sagarpramod01@gmail.com>
2024-11-16 02:28:09 +0530

    updated Jenkins-TF

--- Jenkins-TF/backend.tf
+++ Jenkins-TF/backend.tf
@@ -3,6 +3,8 @@ terraform {
     bucket         = "terraform-bucketv25"
     region         = "ap-south-1"
     key            = "Chatbot-UI/Jenkins-TF/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
   }
   required_version = ">=0.13.0"
 }
--- Jenkins-TF/main.tf
+++ Jenkins-TF/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
-  ami                    = "ami-09b0a86a2c84101e1" #change ami id for different region
+  ami                    = "ami-09b0a86a2c84101e1" 
   instance_type          = "t2.large"
-  subnet_id = aws_subnet.Jenkins-subnet.id
-  key_name               = "ubuntu-node-kp" #change key name as per your setup
+  subnet_id              = aws_subnet.Jenkins-subnet.id
+  key_name               = "ubuntu-node-kp" 
   vpc_security_group_ids = [aws_security_group.Jenkins-VM-SG.id]
   user_data              = file("installations.sh")
 
@@ -18,7 +18,7 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "Jenkins-VM-SG" {
   name        = "Jenkins-VM-SG"
   description = "Allow TLS inbound traffic"
-  vpc_id = aws_vpc.Jenkins-vpc.id
+  vpc_id      = aws_vpc.Jenkins-vpc.id
 
   ingress = [
     for port in [22, 80, 443, 8080, 9000, 3000] : {
@@ -47,8 +47,8 @@ resource "aws_security_group" "Jenkins-VM-SG" {
 }
 #create a vpc
 resource "aws_vpc" "Jenkins-vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
@@ -57,9 +57,9 @@ resource "aws_vpc" "Jenkins-vpc" {
 }
 #Create Subnet 1 
 resource "aws_subnet" "Jenkins-subnet" {
-  vpc_id            = aws_vpc.Jenkins-vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
+  vpc_id                  = aws_vpc.Jenkins-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 
   tags = {
